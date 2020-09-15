03b7efc468e89551edd4af67f645069d2ccad1a0
Your Name <corespiderpro@gmail.com>
2020-09-15 19:22:06 +0530

    Add project files.

---
+++ Winform-PDFtoWORD/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Winform_PDFtoWORD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(699, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Upload PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 422);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Convert to Word";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(210, 46);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(483, 26);
+            this.txtName.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 97);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(876, 319);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 490);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "WinForm-Convert PDF to Word";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Winform-PDFtoWORD/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.apache.pdfbox.pdfviewer;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+using Xceed.Words.NET;
+
+namespace Winform_PDFtoWORD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "PDF files |*.pdf" })
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    txtName.Text = ofd.FileName;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            PDDocument doc = PDDocument.load(txtName.Text);
+            PDFTextStripper stripper = new PDFTextStripper();
+            richTextBox1.Text = (stripper.getText(doc));
+            var docName = Path.GetFileNameWithoutExtension(txtName.Text) + ".docx";
+            var worddoc = DocX.Create(docName);
+            worddoc.InsertParagraph(richTextBox1.Text);
+            worddoc.Save();
+            Process.Start(docName);
+        }
+    }
+}
